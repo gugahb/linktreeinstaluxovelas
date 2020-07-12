10b60aa7c6f930c579900ae953174e4c12b08087
Davi Scholl <davis@bandavide.com.br>
2020-07-12 03:21:42 -0300

    atualizacao

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import Link from '../components/Link'
+import Prismic from 'prismic-javascript'
 
-const Index = () => {
+const Index = props => {
+  const items = props.data.body[0].items
   return (
     <div className='max-w-xl'>
-      <h1>Linktree VIDE</h1>
+      <h1><img src={props.data.headerimg.url} alt='Linktree Agência Vide'/></h1>      
       <ul>
-        <li>
-          <Link href='aaa'>AAA</Link>
-        </li>
-        <li>
-          <Link href='aaa'>BBB</Link>
-        </li>
+        { items.map(item => {
+          return (
+            <li>
+              <Link href={item.link.url}>{item.texto}</Link>
+            </li>
+          )
+        })}       
       </ul>
     </div>
   )
+}
+
+export async function getServerSideProps () {
+  const client = Prismic.client('https://linktreeinsta.cdn.prismic.io/api/v2')
+  const page = await client.getSingle('home')
+  console.log(page)
+  return {
+    props: {
+      data: page.data
+    }
+  }
 }
 
 export default Index
