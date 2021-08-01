5644e03754e5c02dbaccc20fb194a620847274be
Gustavo Borges <gustavohbcn@gmail.com>
2021-07-31 23:42:20 -0300

    Add infos Luxo Velas

--- components/Link.js
+++ components/Link.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Link = ({ href, children }) => {
   return (
     <a href={href} className='max-w-md mx-auto rounded-tl-lg rounded-br-lg bg-gray-800 text-white py-4 w-full block text-center hover:bg-gray-200 hover:text-gray-800 border-2 border-solid border-gray-800 my-4'>
-      {children}
+      {children} 
     </a>
+    
+    
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,32 @@ const Index = props => {
   return (    
     <div className='max-w-md mx-auto'>
       <Head>
-        <title>Agência Vide</title>
+        <title>Luxo velas</title>
       </Head>
 
       {/* <pre>{JSON.stringify(props.data.body[0].primary.titulo[0].text, null, 2)}</pre>*/}
       
-      <img className='mx-auto mt-10 mb-4 rounded-full h-21 w-21 flex items-center justify-center' src={props.data.headerimg.url} width='140px' heigth='140px' alt='Linktree Agência Vide'/>
-      <h1 className='mx-auto text-center mb-8 font-bold text-indigo-700 text-lg'>{titulo}</h1>
+      <img className='mx-auto mt-10 mb-4 rounded-full h-21 w-21 flex items-center justify-center' src="https://cdn.awsli.com.br/400x350/1988/1988382/logo/d7478aac59.png" width='140px' heigth='140px' alt='Linktree Agência Vide'/>
+      <h1 className='mx-auto text-center mb-8 font-bold text-indigo-700 text-lg'>@LuxoVelas</h1>
       <ul>
-        { items.map(item => {
-          return (
+ 
             <li>
-              <Link href={item.link.url}>{item.texto}</Link>
+              <Link href="https://www.luxovelas.com.br/">Site</Link>
             </li>
-          )
-        })}       
+
+            <li>
+              <Link href="https://www.instagram.com/luxo_velas">Instagram</Link>
+            </li>
+            <li>
+              <Link href="mailto:contato@luxovelas.com.br">E-mail</Link>
+            </li>
+            <li>
+              <Link href="">Whatsapp</Link>
+            </li>
+            <li>
+              <Link href="">Facebook</Link>
+            </li>
+
       </ul>
     </div>
   )
